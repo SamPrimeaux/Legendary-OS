4acc78c0a70fa25d5b98da35c556071857cb057f
sam primeaux <inneranimalclothing@gmail.com>
2026-08-22 00:31:56 -0500

    feat(media): mount media routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { CmsWorkspace } from './cms/CmsWorkspace';
+import { MediaAssetPage, MediaWorkspace } from './media';
 import { AppShell } from './shell/AppShell';
 import { PublicCmsPage } from './site/PublicCmsPage';
 
